f0cb16cf316eb5b12fea91416013991e65cdb37c
grnsv <grin.sv@gmail.com>
2022-04-23 17:17:24 +0300

    small fixes

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\NewsController;
-use App\Http\Controllers\OrderController;
 use App\Http\Controllers\SocialController;
 use App\Http\Controllers\SourceController;
 use App\Http\Controllers\FeedbackController;
